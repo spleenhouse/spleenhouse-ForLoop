9d3b206d08e933cffcb5a9120750fded96faf060
spleenhouse <michellepozen@gmail.com>
2023-02-22 19:17:35 +0000

    Completed Lab For Loop

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -42,7 +42,9 @@ public class Lab {
     public String returnNumbers(int start, int end){
         String buildingString = "";
 
-//        code here
+        for(int i = start; i<end; i++){
+            buildingString = buildingString + i + " ";
+        }
 
         return buildingString;
     }
